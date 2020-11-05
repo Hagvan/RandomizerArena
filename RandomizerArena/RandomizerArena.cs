cac937612a70e94e9f8fb9800b4766e6cc724f57
Hagvan <doronindaniel@gmail.com>
2020-11-05 18:47:37 +0200

    Changed 100 starting experience to max skills.

--- RandomizerArena/RandomizerArena.cs
+++ RandomizerArena/RandomizerArena.cs
@@ -123,7 +123,7 @@ namespace RandomizerArena
             player.GiveItem(hat.hat_id, 1);
             player.GiveItem(shirt.shirt_id, 1);
             player.GiveItem(pants.pants_id, 1);
-            player.Experience += 100; // give player 100 exp by default
+            player.MaxSkills();
         }
     }
 }
